479c644cd812f0d9881b182d2e6fa6f0e4d1f719
Thiago-Luiz-casagrande <thiagocasagrande2004@gmail.com>
2025-01-01 17:50:10 -0300

    atualizacao-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar programação",
+      description:
+        "Estudar programação para dse tornar um desenvolvedor full stack",
+      isCompleted: false,
+    },
+  ]);
   return (
-    <div>
-      <h1 className="text-blue-500 text-center">Gerenciador de tarefas</h1>
+    <div className="w-screen h-screen bg-slate-500 flex justfy-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de tarefas
+        </h1>
+      </div>
       <AddTask />
       <Tasks />
     </div>
