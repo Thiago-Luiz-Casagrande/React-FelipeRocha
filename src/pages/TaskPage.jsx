9a8b309382af790454afd83377db8686398eeff3
Thiago-Luiz-casagrande <thiagocasagrande2004@gmail.com>
2025-01-11 19:43:02 -0300

    ajuste-2

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -7,6 +7,7 @@ function TaskPage() {
   const [searchParams] = useSearchParams();
   const title = searchParams.get("title");
   const description = searchParams.get("description");
+
   return (
     <div className="h-screen w-screen bg-slate-500 p-6">
       <div>
